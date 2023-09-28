15975e3202a714b55706e66ecc844ac38278c035
Robert Rosborg <rosborg.robert@gmail.com>
2023-09-28 05:53:27 +0200

    Updated workout_model.dart

--- lib/workout_model.dart
+++ lib/workout_model.dart
@@ -1,8 +1,6 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// A model for workouts consisting of exercises.
 library;
 
-export 'src/workout_model_base.dart';
+export 'src/workout_model_base.dart' show Workout;
+export 'src/exercise_model.dart' show Exercise;
 
-// TODO: Export any libraries intended for clients of this package.
