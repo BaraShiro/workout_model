c2db34fa5c45bdd30c0858598af3b5faac389f8f
Robert Rosborg <rosborg.robert@gmail.com>
2023-09-28 06:06:33 +0200

    Added simple example code

--- example/workout_model_example.dart
+++ example/workout_model_example.dart
@@ -1,5 +1,6 @@
 import 'package:workout_model/workout_model.dart';
 
 void main() {
-
+  Exercise exercise = Exercise("asdfasdf", "An exercise", "Some exercise that can be done", 4, 1, 5, 20.0);
+  print("Id: ${exercise.id}, Name: ${exercise.name}, Description: ${exercise.description}, Repetitions: ${exercise.numberOfRepetitions}, Rest Time: ${exercise.restTimeInMinutes}, Sets: ${exercise.numberOfSets}, Weight: ${exercise.weightInKilograms}");
 }
