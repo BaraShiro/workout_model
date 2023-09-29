1a8598488b8c9afe5674ab5ced2eafc5a365bcc8
Robert Rosborg <rosborg.robert@gmail.com>
2023-09-29 10:21:54 +0200

    Added some exceptions

---
+++ lib/src/exceptions.dart
@@ -0,0 +1,29 @@
+/// Exception thrown if a name field is empty.
+class EmptyNameException implements Exception {
+  final String? msg;
+
+  const EmptyNameException([this.msg]);
+
+  @override
+  String toString() => msg ?? 'EmptyNameException';
+}
+
+/// Exception thrown if a number is not allowed to be non positive, i.e. < 1.
+class NonPositiveNumberException implements Exception {
+  final String? msg;
+
+  const NonPositiveNumberException([this.msg]);
+
+  @override
+  String toString() => msg ?? 'NonPositiveNumberException';
+}
+
+/// Exception thrown if a number is not allowed to be negative, i.e. < 0.
+class NegativeNumberException implements Exception {
+  final String? msg;
+
+  const NegativeNumberException([this.msg]);
+
+  @override
+  String toString() => msg ?? 'NegativeNumberException';
+}
