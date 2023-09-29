9a6f6a8ad142f4cec5e16c8210360f9656909cb1
Robert Rosborg <rosborg.robert@gmail.com>
2023-09-29 10:23:08 +0200

    Added exceptions to Exercise

--- lib/src/exercise_model.dart
+++ lib/src/exercise_model.dart
@@ -1,4 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:workout_model/src/exceptions.dart';
 import 'package:workout_model/src/i_identifiable.dart';
 
 part 'exercise_model.freezed.dart';
@@ -26,6 +27,10 @@ class Exercise with _$Exercise implements IIdentifiable {
   }) = _Exercise;
 
   /// Constructs a new [Exercise] object and sets [uuid] to a random v4 UUID.
+  ///
+  /// Throws [EmptyNameException] if [name] is empty.
+  /// Throws [NonPositiveNumberException] if [numberOfRepetitions] or [numberOfSets] is a non positive number.
+  /// Throws [NegativeNumberException] if [restTimeInMinutes] or [weightInKilograms] is a negative number.
   factory Exercise({
     required String name,
     required String description,
@@ -34,6 +39,12 @@ class Exercise with _$Exercise implements IIdentifiable {
     required int numberOfSets,
     required double weightInKilograms,
   }) {
+    if(name.isEmpty) throw EmptyNameException("Name can not be empty.");
+    if(numberOfRepetitions < 1) throw NonPositiveNumberException("Number of repetitions must be a positive integer.");
+    if(restTimeInMinutes < 0) throw NegativeNumberException("Rest time in minutes must be a non negative integer.");
+    if(numberOfSets < 1) throw NonPositiveNumberException("Number of sets must be a positive integer.");
+    if(weightInKilograms < 0) throw NegativeNumberException("Weight in kilograms must be a non negative number.");
+
     return _Exercise(
       uuid: IIdentifiable.generateUuid(),
       name: name,
@@ -50,8 +61,11 @@ class Exercise with _$Exercise implements IIdentifiable {
   => _$ExerciseFromJson(json);
 
   /// Updates the [name] of this [Exercise]
+  ///
+  /// [newName] must be non empty, otherwise throws [EmptyNameException].
   Exercise updateName(String newName)
   {
+    if(newName.isEmpty) throw EmptyNameException("Name can not be empty.");
     return copyWith(name: newName);
   }
 
@@ -62,26 +76,38 @@ class Exercise with _$Exercise implements IIdentifiable {
   }
 
   /// Updates the [numberOfRepetitions] of this [Exercise]
+  ///
+  /// [newNumberOfRepetitions] must be a positive integer, otherwise throws [NonPositiveNumberException].
   Exercise updateNumberOfRepetitions(int newNumberOfRepetitions)
   {
+    if(newNumberOfRepetitions < 1) throw NonPositiveNumberException("Number of repetitions must be a positive integer.");
     return copyWith(numberOfRepetitions: newNumberOfRepetitions);
   }
 
   /// Updates the [restTimeInMinutes] of this [Exercise]
+  ///
+  /// [newRestTimeInMinutes] must be a non negative integer, otherwise throws [NegativeNumberException].
   Exercise updateRestTimeInMinutes(int newRestTimeInMinutes)
   {
+    if(newRestTimeInMinutes < 0) throw NegativeNumberException("Rest time in minutes must be a non negative integer.");
     return copyWith(restTimeInMinutes: newRestTimeInMinutes);
   }
 
   /// Updates the [numberOfSets] of this [Exercise]
+  ///
+  /// [newNumberOfSets] must be a positive integer, otherwise throws [NonPositiveNumberException].
   Exercise updateNumberOfSets(int newNumberOfSets)
   {
+    if(newNumberOfSets < 1) throw NonPositiveNumberException("Number of sets must be a positive integer.");
     return copyWith(numberOfSets: newNumberOfSets);
   }
 
   /// Updates the [weightInKilograms] of this [Exercise]
+  ///
+  /// [newWeightInKilograms] must be a non negative number, otherwise throws [NegativeNumberException].
   Exercise updateWeightInKilograms(double newWeightInKilograms)
   {
+    if(newWeightInKilograms < 0) throw NegativeNumberException("Weight in kilograms must be a non negative number.");
     return copyWith(weightInKilograms: newWeightInKilograms);
   }
 }
