f436082be74cdb5900a9365fe15e595ca8b63638
Robert Rosborg <rosborg.robert@gmail.com>
2023-09-30 11:34:09 +0200

    Updated example

--- example/workout_model_example.dart
+++ example/workout_model_example.dart
@@ -1,97 +1,122 @@
+import 'dart:convert';
 import 'dart:io';
+import 'package:workout_model/src/hive_repository.dart';
 import 'package:workout_model/workout_model.dart';
 
-void main() {
+void main() async {
+  final String path = Directory.current.path;
+  final JsonEncoder encoder = JsonEncoder.withIndent('  ');
+  final Duration duration = const Duration(seconds: 1);
+
+  print("Creating repositories...");
+  final ExerciseRepository exerciseRepository = ExerciseRepository.instance;
+  final WorkoutRepository workoutRepository = WorkoutRepository.instance;
+  await exerciseRepository.initialize(filePath: path);
+  await workoutRepository.initialize(filePath: path);
+  print("Done!");
+
+  print("");
+  print("Create an exercise and store it, read it back and print it. Print any errors.");
   Exercise exercise1 = Exercise(
     name:"An exercise",
     description: "Some exercise that can be done",
     numberOfRepetitions: 4,
     restTimeInMinutes: 1,
     numberOfSets: 5,
-    weightInKilograms: 20.0);
+    weightInKilograms: 20.0
+  );
 
-  Exercise exercise2 = exercise1
+  await exerciseRepository.create(exercise1).then((value) =>
+      value.match(
+              (l) => print(l.toString()),
+              (r) => null
+      )
+  );
+
+  await exerciseRepository.read(exercise1.uuid).then((value) =>
+      value.match(
+              (error) => print(error.toString()),
+              (exercise) => print(encoder.convert(exercise.toJson()))
+      )
+  );
+
+  print("");
+  print("Copy and modify an exercise and store it, read it back and print it. Print any errors.");
+  Exercise exercise2 = Exercise.copy(exercise1);
+  exercise2 = exercise2
       .updateName("An updated exercise")
       .updateNumberOfRepetitions(8);
 
-  Exercise exercise3 = Exercise(
-    name: "Some exercise",
-    description: "An exercise that's fun to do",
-    numberOfRepetitions: 7,
-    restTimeInMinutes: 1,
-    numberOfSets: 3,
-    weightInKilograms: 2.5);
+  await exerciseRepository.create(exercise2).then((value) =>
+      value.match(
+              (error) => print(error.toString()),
+              (exercise) => null
+      )
+  );
 
-  Exercise exercise4 = Exercise.fromJson(exercise3.toJson());
-
-  print(exercise1.toJson());
-  print(exercise2.toJson());
-  print(exercise3.toJson());
-  print(exercise4.toJson());
+  await exerciseRepository.read(exercise2.uuid).then((value) =>
+      value.match(
+              (error) => print(error.toString()),
+              (exercise) => print(encoder.convert(exercise.toJson()))
+      )
+  );
 
   print("");
-
+  print("Create a workout, add both exercises, store it, read it back and print it. Print any errors.");
   Workout workout1 = Workout(exercises: <Exercise>[]);
-  Workout workout2 = workout1.updateExercises(exercise1);
-  Workout workout3 = Workout.fromJson(workout2.toJson());
+  workout1 = workout1.updateExercisesWithSeveral([exercise1, exercise2]);
 
-  print(workout1.toJson());
-  print(workout2.toJson());
-  print(workout3.toJson());
+  await workoutRepository.create(workout1).then((value) =>
+      value.match(
+              (error) => print(error.toString()),
+              (workout) => null
+      )
+  );
 
-  print("");
-
-  Workout workout2copy = Workout.copy(baseWorkout: workout2);
-  print(workout2.toJson());
-  print(workout2copy.toJson());
-
-  print("");
-
-  workout2 = workout2.updateExercises(exercise3);
-  print(workout2.toJson());
-  workout2 = workout2.removeExerciseAtIndex(0);
-  print(workout2.toJson());
+  await workoutRepository.read(workout1.uuid).then((value) =>
+      value.match(
+              (error) => print(error.toString()),
+              (workout) => print(encoder.convert(workout.toJson()))
+      )
+  );
 
   print("");
+  print("Start the workout, pause and resume it a couple of times, and then stop. Print the result.");
 
-  Duration duration = const Duration(seconds: 1);
 
   print('Starting workout');
-  Workout workout4 = workout1.startWorkout();
-  sleep(duration);
-  workout4 = workout4.stopWorkout();
-  print('Worked out for 1 second');
-  print('Stopping workout');
-  print(workout4.toJson());
-
-  print("");
-
-  print('Starting workout');
-  Workout workout5 = workout1.startWorkout();
+  workout1 = workout1.startWorkout();
   sleep(duration);
   print('Worked out for 1 second');
 
   print('Pausing workout');
-  workout5 = workout5.pauseWorkout();
+  workout1 = workout1.pauseWorkout();
   sleep(duration);
   print('Paused for 1 second');
 
   print('Resuming workout');
-  workout5 = workout5.resumeWorkout();
+  workout1 = workout1.resumeWorkout();
   sleep(duration);
   print('Worked out for 1 second');
 
   print('Pausing workout');
-  workout5 = workout5.pauseWorkout();
+  workout1 = workout1.pauseWorkout();
   sleep(duration);
   print('Paused for 1 second');
 
   print('Resuming workout');
-  workout5 = workout5.resumeWorkout();
+  workout1 = workout1.resumeWorkout();
   sleep(duration);
   print('Worked out for 1 second');
   print('Stopping workout');
-  workout5 = workout5.stopWorkout();
+  workout1 = workout1.stopWorkout();
 
-  print(workout5.toJson());
+  print(encoder.convert(workout1.toJson()));
+
+  print("");
+  print("Cleaning up...");
+  await exerciseRepository.destroy();
+  await workoutRepository.destroy();
+  print("Done!");
+
 }
--- lib/workout_model.dart
+++ lib/workout_model.dart
@@ -4,4 +4,5 @@ library;
 export 'src/workout_model_base.dart' show Workout;
 export 'src/exercise_model.dart' show Exercise;
 export 'src/exceptions.dart';
+export 'src/hive_repository.dart';
 
